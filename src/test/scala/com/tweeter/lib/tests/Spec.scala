bec46db39da6ae64dbaf231026b7b3b238163bc2
Carlos Vasquez <hispanasian@gmail.com>
2014-12-20 03:56:41 -0500

    refactored testing spec one last time

--- src/test/scala/com/tweeter/lib/tests/Spec.scala
+++ src/test/scala/com/tweeter/lib/tests/Spec.scala
@@ -3,16 +3,16 @@ package com.tweeter.lib.tests
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{FlatSpec, FlatSpecLike, BeforeAndAfterAll}
+import org.scalatest.{FlatSpecLike, BeforeAndAfterAll}
 
 /**
  * The class to be extended by any tests.
  * Created by Carlos on 12/18/2014.
  */
-abstract class UnitSpec extends FlatSpec with MockFactory with BeforeAndAfterAll
+trait UnitSpec extends FlatSpecLike with MockFactory with BeforeAndAfterAll
 
 /**
  * The class to be extended by an Akka related tests
  * @param _system
  */
-abstract class AkkaSpec(_system:ActorSystem) extends TestKit(_system) with FlatSpecLike with MockFactory with BeforeAndAfterAll
+abstract class AkkaSpec(_system:ActorSystem) extends TestKit(_system) with UnitSpec

a3f0f46506fe73cd175af37ee9c89d305d3fef10
Carlos Vasquez <hispanasian@gmail.com>
2014-12-20 12:16:10 -0500

    tested FollowerWorker

--- src/test/scala/com/tweeter/module/relationship/follower/FollowerWorkerTest.scala
+++ src/test/scala/com/tweeter/module/relationship/follower/FollowerWorkerTest.scala
@@ -1,6 +1,6 @@
 package com.tweeter.module.relationship.follower
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{ActorSystem}
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import com.tweeter.lib.cache.Cache
 import com.tweeter.lib.tests.{AkkaSpec}
@@ -25,7 +25,7 @@ class FollowerWorkerTest(_system:ActorSystem) extends AkkaSpec(_system)
 
   override def afterAll { TestKit.shutdownActorSystem(system) }
 
-  "A FollowerWorker" should "return an empty list when  a new FollowerWorker is sent a getFollowers message" in
+  "A FollowerWorker" should "return an empty list when a new FollowerWorker is sent a getFollowers message" in
     {
       val f = fixture
       (f.cache.get _) expects(10) returning(None)
@@ -33,8 +33,18 @@ class FollowerWorkerTest(_system:ActorSystem) extends AkkaSpec(_system)
       worker ! Envelope(GetFollowers(User(10)), null, f.handler.ref)
       f.handler.expectMsg(Envelope(Followers(User(10), List[User]()), null, f.handler.ref))
     }
-  it should "" in
+  it should "add User(10) as a follower of User(5) when it is sent AddFollower(User(5),User(10))" in
     {
-
+      val f = fixture
+      (f.cache.+= _) expects(5,User(10))
+      val worker = TestActorRef(new FollowerWorker(f.cache))
+      worker ! Envelope(AddFollower(User(5),User(10)), null, f.handler.ref)
+    }
+  it should "remove User(10) as a follower of User(5) when it is sent RemoveFollower(User(5),User(10))" in
+    {
+      val f = fixture
+      (f.cache.remove _) expects(5,10)
+      val worker = TestActorRef(new FollowerWorker(f.cache))
+      worker ! Envelope(RemoveFollower(User(5),User(10)), null, f.handler.ref)
     }
 }
